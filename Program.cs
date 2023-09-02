7e1cc50f97f74ee83f1dee7ae3b7259a48af199f
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-02 20:55:25 +0530

    [aditya]: Check Employee

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CheckEmp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int Emp = random.Next(0, 2);
+            int Is_Full_Time = 0;
+            if (Emp == Is_Full_Time)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is not Present");
+            }
+        }
+    }
+}
