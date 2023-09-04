f23ffe763abcde01630488a819c4e1f70463afcb
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-04 18:27:24 +0530

    [aditya]: Compute Employee Wage and Save Total Wage By Company

---
+++ EmpWageBuilderObject.cs
@@ -0,0 +1,70 @@
+﻿namespace ConsoleApp2
+{
+    class EmpWageBuilderObject
+    {
+        public const int Is_Part_Time = 1;
+        public const int Is_Full_Time = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numofWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage=0;
+
+        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numofWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+
+        }
+        public void computeEmpWage()
+        {
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numofWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case Is_Part_Time:
+                        empHrs = 4;
+                        break;
+
+                    case Is_Full_Time:
+                        empHrs = 8;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day# :" + totalWorkingDays + " Emp Hrs : " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage :" + company + " is: " + totalEmpWage);
+        }
+
+        public string toString()
+        {
+            return "Total Emp Wage for company : " + this.company + " is: " + this.totalEmpWage;
+        }
+
+        static void Main(string[] args)
+        {
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+
+        }
+    }
+}
