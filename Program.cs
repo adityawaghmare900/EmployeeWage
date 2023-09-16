31d3980c6003e55f3b3993d651d6347dc0dd2d2c
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-16 23:07:47 +0530

    [aditya]: emp wae for multiple company using interface

--- Program.cs
+++ Program.cs
@@ -1,46 +1,15 @@
-﻿namespace ConsoleApp2
-{
-    class Program
-    {
-        public const int Is_Part_Time = 1;
-        public const int Is_Full_Time = 2;
-        
-        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-        {
-            int empHrs = 0;
-            int totalWorkingDays = 0; 
-            int totalEmpHrs = 0;
-            
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case Is_Part_Time:
-                        empHrs = 4;
-                        break;
+﻿using EmployeeWage;
 
-                    case Is_Full_Time:
-                        empHrs = 8;
-                        break;
-                    
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day# :" + totalWorkingDays + " Emp Hrs : " + empHrs);
-                }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
-            return totalEmpWage;
-        }     
-       static void Main(string[] args)
+namespace CheckEmp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
         {
-            computeEmpWage("Vi", 20,30,100);
-            computeEmpWage("Jio",20,20,30);
+            EmpWageBuilderArray empWageBuilder=new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
